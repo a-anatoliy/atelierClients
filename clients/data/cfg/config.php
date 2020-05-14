8ba075861a9f3f79812bc57c2c31cce494c6079a
Anatol <a3three@gmail.com>
2020-05-14 22:28:22 +0200

    adding "clients" directory

---
+++ clients/data/cfg/config.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+    'site' => array(
+        'url'     => 'http://luckydress.eu/',
+        'defLang' => 'en',
+        'orgName' => 'Lucky DRESS Atelier',
+        'theme'   => 'default',
+    ),
+    'stat' => array(
+        'rowsPerPage' => 30,
+        'urlPattern'  => '/bin/mr450.php?s56=(:num)'
+     ),
+    'clients' => array(
+        131470 => array( 'name'=>'Ehjae', 'email'=>'ehjae@gmail.com', 'secret'=>'2652875' ),
+        133234 => array( 'name'=>'Emilia', 'email'=>'emely@gmail.com', 'secret'=>'2652875' ),
+     ),
+    'form' => array(
+    'to'   => 'luckydress77@gmail.com',
+    'cc'   => 'a3three@gmail.com',
+    'bcc'  => '',
+    'subject' => 'Lucky DRESS HowTos visitor'
+    ),
+);
+
+
